f4dd73ebe818e3ae9113b92c3bf3bceac9913d87
mprecilio <84741853+mprecilio@users.noreply.github.com>
2021-10-16 15:27:07 -0500

    App setup complete

---
+++ ToDoBack/src/main/java/example/dao/AppDao.java
@@ -0,0 +1,11 @@
+package example.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import example.models.UserModel;
+
+@Repository
+public interface AppDao extends JpaRepository<UserModel, Integer> {
+
+}
